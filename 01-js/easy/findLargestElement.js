4a6f86a35c63c11a0377d93ca365ca620f6df23b
ankurpm <ankurpm90@gmail.com>
2024-01-07 22:13:44 -0500

    week1: easy :findlargestnumber.js

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -7,6 +7,18 @@
 
 function findLargestElement(numbers) {
     
+    if(numbers.length === 1){
+        return numbers[0]
+    }
+    let largest = numbers[0]
+    for(let i=1; i<numbers.length; i++){
+        if(numbers[i] > largest){
+            largest = numbers[i]
+        }
+
+    }
+    return largest;
+    
 }
 
 module.exports = findLargestElement;
