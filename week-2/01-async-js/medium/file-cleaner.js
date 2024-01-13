3c87fe291d05132d33f0ed08603cbe9aa86956a6
ankurpm <ankurpm90@gmail.com>
2024-01-12 23:04:15 -0500

    week2: medium: filecleaner.js without regex

--- week-2/01-async-js/medium/file-cleaner.js
+++ week-2/01-async-js/medium/file-cleaner.js
@@ -1,7 +1,28 @@
 const fs = require('fs');
 
-let arr = fs.readFile("spacedFile.txt", "utf-8",(err, data) => {
-    console.log(data.split(/\s+/).join(' '))
+fs.readFile("spacedFile.txt", "utf-8", (err, data) => {
+    var temp = ''
+    var tempArr = []
+    for (var i = 0; i < data.length; i++) {
+        if (data[i] != ' ') {
+            temp = temp + data[i]
+        } else {
+            if (temp != '') {
+                tempArr.push(temp)
+            }
+            temp = ''
+        }
+    }
+    var finalString = ''
+
+    for (var i = 0; i < tempArr.length; i++) {
+        if (i === 0) {
+            finalString = finalString + tempArr[i]
+        } else {
+            finalString = finalString + ' ' + tempArr[i]
+        }
+    }
+    fs.writeFile('finecleaneroutput.txt', finalString, (err) => { })
 });
 
 
