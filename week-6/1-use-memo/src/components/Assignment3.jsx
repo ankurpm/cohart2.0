15535d60ecf7966d9600014dca48909bda529362
ankurpm <37891760+ankurpm@users.noreply.github.com>
2024-08-27 19:35:08 -0400

    week 6: Assignment 3 solution

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -12,7 +12,10 @@ export const Assignment3 = () => {
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo( ()=>{
+        return items.reduce( (accumulator, item)=> accumulator+item.value, 0)
+
+    }, [items])
     // Your code ends here
     return (
         <div>
